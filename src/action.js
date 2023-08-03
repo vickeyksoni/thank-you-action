d8b94147f41c8a4189b7a18f3ced53bf43f85cea
Soni <vickey.kumar.soni01@sap.com>
2023-08-03 22:12:07 +0530

    adding new

--- src/action.js
+++ src/action.js
@@ -8,9 +8,9 @@
 //     setOutputs,
 //   } from "./helpers";
   const currentDate = new Date();
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
+  const day = String(currentDate.getDate()).padStart(2, '0');
+  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+  const year = currentDate.getFullYear();
   const currentDateString = `${day}-${month}-${year}`;
 
 
